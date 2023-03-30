425e3da4524f9093d3feb7ab67bac223acfc9208
NOMAN1802 <mustakimnoman82@gmail.com>
2023-03-30 16:22:49 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button/Button';
+import Card from './components/Card/Card';
+import Header from './components/Header/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Card></Card>
+      
+      
+    </>
+  );
+};
+
+export default App;
